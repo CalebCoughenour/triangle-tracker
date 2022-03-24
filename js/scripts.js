e232e204521ee41a0798cca40f705a11027fd812
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-03-24 15:07:59 -0700

    finish the scripts for triangle types.
    Co-authored-by: Tony Martinez <tony.martinez5897@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
   $("form#triangleForm").submit(function(event) {
+    event.preventDefault();
     const sideA = parseInt($("input#sideA").val());
     const sideB = parseInt($("input#sideB").val());
     const sideC = parseInt($("input#sideC").val());
 
 
-  if (sideA === sideB && sideB===sideC) {
-    alert("That's an EQUILATERAL TRIANGLE");
-  }
-
-  event.preventDefault();
-});
+    if (sideA + sideB <= sideC || sideB + sideC <= sideA || sideC + sideA <= sideB) {
+      alert("That ain't a triangle!");  
+  } else if (sideA === sideB && sideB === sideC) {
+      alert("That's an EQUILATERAL TRIANGLE");
+  } else if (sideA === sideB || sideC === sideB || sideC ===sideA) {
+      alert("That's an Isosceles");
+  } else if (sideA != sideB && sideB != sideC)  {
+      alert("That's an SCALENE");
+  } else {
+      alert("Where'd you get that triangle???");
+  }  
+  });
 });
